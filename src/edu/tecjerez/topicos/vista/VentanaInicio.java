c230fab68bf7ee5ff84af49187ee832c99730640
Jose Luis Sanchez <jose.sr@tecjerez.edu.mx>
2025-05-26 00:32:53 -0600

    Se agregó la Ventana con componentes de Jframes, como JMenu y toolbars

--- src/edu/tecjerez/topicos/vista/VentanaInicio.java
+++ src/edu/tecjerez/topicos/vista/VentanaInicio.java
@@ -17,16 +17,53 @@ public class VentanaInicio extends JFrame implements ActionListener {
     JMenuItem itemAltasTraficadores, itemBajasTraficadores, itemModificacionesTraficadores, itemConsultasTraficadores;
     //--------------------
 
+    JToolBar toolbar;
+
+    JButton btnToolbarSalir, btnToolbarLogin;
+
     public VentanaInicio(){
         setLayout(new BorderLayout());
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setTitle("Dane County Airport");
-        setSize(1600, 1000);
+        setExtendedState(JFrame.MAXIMIZED_BOTH);
         setLocationRelativeTo(null);
         getContentPane().setBackground(new Color(31, 70, 96));
         setVisible(true);
 
         JMenuBar menuBar = new JMenuBar();
+
+
+//----------------------- MENU AVIONES-------------------–––––––---––––––––--------
+        menuAviones = new JMenu("Aviones");
+        menuAviones.setMnemonic(KeyEvent.VK_W);
+
+        itemAltasTraficadores = new JMenuItem("Agregar");
+        itemAltasTraficadores.setMnemonic(KeyEvent.VK_A);
+        itemAltasTraficadores.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_A, ActionEvent.CTRL_MASK));
+        menuAviones.add(itemAltasTraficadores);
+        itemAltasTraficadores.addActionListener(this);
+
+        itemBajasTraficadores = new JMenuItem("Eliminar");
+        itemBajasTraficadores.setMnemonic(KeyEvent.VK_E);
+        itemBajasTraficadores.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_E, ActionEvent.CTRL_MASK));
+        menuAviones.add(itemBajasTraficadores);
+        itemBajasTraficadores.addActionListener(this);
+
+        itemModificacionesTraficadores = new JMenuItem("Modificar");
+        itemModificacionesTraficadores.setMnemonic(KeyEvent.VK_M);
+        itemModificacionesTraficadores.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_M, ActionEvent.CTRL_MASK));
+        menuAviones.add(itemModificacionesTraficadores);
+        itemModificacionesTraficadores.addActionListener(this);
+
+        itemConsultasTraficadores = new JMenuItem("Buscar");
+        itemConsultasTraficadores.setMnemonic(KeyEvent.VK_B);
+        itemConsultasTraficadores.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_B, ActionEvent.CTRL_MASK));
+        menuAviones.add(itemConsultasTraficadores);
+        itemConsultasTraficadores.addActionListener(this);
+
+        menuBar.add(menuAviones);
+
+        //------------- final menu aviones -----------
 
 //----------------------- MENU EMPLEADOS ----------------------------
             menuEmpleados = new JMenu("Empleados");
@@ -35,86 +72,68 @@ public class VentanaInicio extends JFrame implements ActionListener {
                     menuTecnicos.setMnemonic(KeyEvent.VK_Q);
 
                         itemAltasTecnicos = new JMenuItem("Agregar");
-                        itemAltasTecnicos.setMnemonic(KeyEvent.VK_A);
-                        itemAltasTecnicos.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_A, ActionEvent.CTRL_MASK));
                         menuTecnicos.add(itemAltasTecnicos);
                         itemAltasTecnicos.addActionListener(this);
 
                         itemBajasTecnicos = new JMenuItem("Eliminar");
-                        itemBajasTecnicos.setMnemonic(KeyEvent.VK_E);
-                        itemBajasTecnicos.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_E, ActionEvent.CTRL_MASK));
                         menuTecnicos.add(itemBajasTecnicos);
                         itemBajasTecnicos.addActionListener(this);
 
                         itemModificacionesTecnicos = new JMenuItem("Modificar");
-                        itemModificacionesTecnicos.setMnemonic(KeyEvent.VK_M);
-                        itemModificacionesTecnicos.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_M, ActionEvent.CTRL_MASK));
                         menuTecnicos.add(itemModificacionesTecnicos);
                         itemModificacionesTecnicos.addActionListener(this);
 
                         itemConsultasTecnicos = new JMenuItem("Buscar");
-                        itemConsultasTecnicos.setMnemonic(KeyEvent.VK_C);
-                        itemConsultasTecnicos.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_B, ActionEvent.CTRL_MASK));
                         menuTecnicos.add(itemConsultasTecnicos);
                         itemConsultasTecnicos.addActionListener(this);
 
                 menuEmpleados.add(menuTecnicos);
+
+                        menuTraficadores= new JMenu("Controladores Aéreos");
+
+                        itemAltasTraficadores = new JMenuItem("Agregar");
+                        menuTraficadores.add(itemAltasTraficadores);
+                        itemAltasTraficadores.addActionListener(this);
+
+                        itemBajasTraficadores = new JMenuItem("Eliminar");
+                        menuTraficadores.add(itemBajasTraficadores);
+                        itemBajasTraficadores.addActionListener(this);
+
+                        itemModificacionesTraficadores = new JMenuItem("Modificar");
+                        menuTraficadores.add(itemModificacionesTraficadores);
+                        itemModificacionesTraficadores.addActionListener(this);
+
+                        itemConsultasTraficadores = new JMenuItem("Buscar");
+                        menuTraficadores.add(itemConsultasTraficadores);
+                        itemConsultasTraficadores.addActionListener(this);
+
+                        menuEmpleados.add(menuTraficadores);
+
             menuBar.add(menuEmpleados);
 
         setJMenuBar(menuBar);
 
+//----------- final menu empleados-----------------
 
+        toolbar = new JToolBar();
+            ImageIcon logoIcono = new ImageIcon("./imagenes/toolbarLogo.png");
+            JLabel logoLabel = new JLabel(logoIcono);
+            logoLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+            toolbar.add(logoLabel);
 
-            //----------------------- MENU AVIONES-------------------–––––––---––––––––--------
-            menuAviones = new JMenu("Aviones");
-            menuAviones.setMnemonic(KeyEvent.VK_W);
+            //btn para salir --------------
+            btnToolbarSalir = new JButton();
+            btnToolbarSalir.setIcon(new ImageIcon("./imagenes/toolbarSalir.png"));
+            toolbar.add(btnToolbarSalir);
+            btnToolbarSalir.addActionListener(this);
+            btnToolbarSalir.setToolTipText("Salir del programa.");
 
-                itemAltasTraficadores = new JMenuItem("Agregar");
-                itemAltasTraficadores.setMnemonic(KeyEvent.VK_R);
-                itemAltasTraficadores.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_R, ActionEvent.CTRL_MASK));
-                menuAviones.add(itemAltasTraficadores);
-                itemAltasTraficadores.addActionListener(this);
+            //btn para regresar al login ---------
+            btnToolbarLogin = new JButton();
+            btnToolbarLogin.setIcon(new ImageIcon("./imagenes/toolbarLogin.png"));
 
-                itemBajasTraficadores = new JMenuItem("Eliminar");
-                itemBajasTraficadores.setMnemonic(KeyEvent.VK_F);
-                itemBajasTraficadores.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_F, ActionEvent.CTRL_MASK));
-                menuAviones.add(itemBajasTraficadores);
-                itemBajasTraficadores.addActionListener(this);
-
-                itemModificacionesTraficadores = new JMenuItem("Modificar");
-                itemModificacionesTraficadores.setMnemonic(KeyEvent.VK_T);
-                itemModificacionesTraficadores.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_T, ActionEvent.CTRL_MASK));
-                menuAviones.add(itemModificacionesTraficadores);
-                itemModificacionesTraficadores.addActionListener(this);
-
-                itemConsultasTraficadores = new JMenuItem("Buscar");
-                itemConsultasTraficadores.setMnemonic(KeyEvent.VK_G);
-                itemConsultasTraficadores.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_G, ActionEvent.CTRL_MASK));
-                menuAviones.add(itemConsultasTraficadores);
-                itemConsultasTraficadores.addActionListener(this);
-
-            menuBar.add(menuAviones);
-
-//para agregar la barra de
-        setJMenuBar(menuBar);
-
-/*
-anotaciones:
-
-Tablas que en teoría debe de tener el proyecto:
-
-1. tecnicos: (aunque la duda es considerarlo dentro de empleados, porque el tecnico tendría mas elementos de especialización
-que un empleado)
-
-2. Aviones: (esta tabla registra todos los aviones el numero de modelo de avion y los tecnicos que se especializan en esos modelos,
-la fecha de la utlima examinación de avion y el nombre del tecnico que la examino)
-
-3. Empleados / Union: (En estre se registra todo el personal y el puesto que ocupan, por ello mi duda de poner a los tecnicos aqui
-o darle su propio espacio a ellos)
- */
-
-
-
+            add(toolbar, BorderLayout.PAGE_START);
+            toolbar.setVisible(true);
     }
 
     @Override
