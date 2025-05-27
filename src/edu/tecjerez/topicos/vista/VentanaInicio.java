90415d412c1196cabcbb0ccc24d496a7e0e34e45
Jose Luis Sanchez <jose.sr@tecjerez.edu.mx>
2025-05-27 00:09:21 -0600

    todos los componentes visuales en AltasAviones han sido añadidos

--- src/edu/tecjerez/topicos/vista/VentanaInicio.java
+++ src/edu/tecjerez/topicos/vista/VentanaInicio.java
@@ -1,5 +1,7 @@
 package edu.tecjerez.topicos.vista;
 
+import edu.tecjerez.topicos.vista.aviones.AltasAviones;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -7,6 +9,7 @@ import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 
 public class VentanaInicio extends JFrame implements ActionListener {
+    private JDesktopPane ventanaDesktopPane;
 
     JMenu menuEmpleados, menuAviones;
     //-------------- ITEMS EMPLEADOS ---------------
@@ -15,13 +18,14 @@ public class VentanaInicio extends JFrame implements ActionListener {
     //-------------- ITEMS DE LOS ITEMS EMPLEADOS ---------------------
     JMenuItem itemAltasTecnicos, itemBajasTecnicos, itemModificacionesTecnicos, itemConsultasTecnicos;
     JMenuItem itemAltasTraficadores, itemBajasTraficadores, itemModificacionesTraficadores, itemConsultasTraficadores;
+    JMenuItem itemAltasAviones, itemBajasAviones, itemModificacionesAviones, itemConsultasAviones;
     //--------------------
 
     JToolBar toolbar;
 
     JButton btnToolbarSalir, btnConsulta, btnAgregar, btnModificar, btnBorrar;
 
-    public VentanaInicio(){
+    public VentanaInicio() {
         setLayout(new BorderLayout());
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setTitle("Dane County Airport");
@@ -38,80 +42,78 @@ public class VentanaInicio extends JFrame implements ActionListener {
         menuAviones = new JMenu("Aviones");
         menuAviones.setMnemonic(KeyEvent.VK_W);
 
-        itemAltasTraficadores = new JMenuItem("Agregar");
-        itemAltasTraficadores.setMnemonic(KeyEvent.VK_A);
-        itemAltasTraficadores.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_A, ActionEvent.CTRL_MASK));
-        menuAviones.add(itemAltasTraficadores);
-        itemAltasTraficadores.addActionListener(this);
+        itemAltasAviones = new JMenuItem("Agregar");
+        itemAltasAviones.setMnemonic(KeyEvent.VK_A);
+        itemAltasAviones.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_A, ActionEvent.CTRL_MASK));
+        menuAviones.add(itemAltasAviones);
+        itemAltasAviones.addActionListener(this);
 
-        itemBajasTraficadores = new JMenuItem("Eliminar");
-        itemBajasTraficadores.setMnemonic(KeyEvent.VK_E);
-        itemBajasTraficadores.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_E, ActionEvent.CTRL_MASK));
-        menuAviones.add(itemBajasTraficadores);
-        itemBajasTraficadores.addActionListener(this);
+        itemBajasAviones = new JMenuItem("Eliminar");
+        itemBajasAviones.setMnemonic(KeyEvent.VK_E);
+        itemBajasAviones.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_E, ActionEvent.CTRL_MASK));
+        menuAviones.add(itemBajasAviones);
+        itemBajasAviones.addActionListener(this);
 
-        itemModificacionesTraficadores = new JMenuItem("Modificar");
-        itemModificacionesTraficadores.setMnemonic(KeyEvent.VK_M);
-        itemModificacionesTraficadores.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_M, ActionEvent.CTRL_MASK));
-        menuAviones.add(itemModificacionesTraficadores);
-        itemModificacionesTraficadores.addActionListener(this);
+        itemModificacionesAviones = new JMenuItem("Modificar");
+        itemModificacionesAviones.setMnemonic(KeyEvent.VK_M);
+        itemModificacionesAviones.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_M, ActionEvent.CTRL_MASK));
+        menuAviones.add(itemModificacionesAviones);
+        itemModificacionesAviones.addActionListener(this);
 
-        itemConsultasTraficadores = new JMenuItem("Buscar");
-        itemConsultasTraficadores.setMnemonic(KeyEvent.VK_B);
-        itemConsultasTraficadores.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_B, ActionEvent.CTRL_MASK));
-        menuAviones.add(itemConsultasTraficadores);
-        itemConsultasTraficadores.addActionListener(this);
+        itemConsultasAviones = new JMenuItem("Buscar");
+        itemConsultasAviones.setMnemonic(KeyEvent.VK_B);
+        itemConsultasAviones.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_B, ActionEvent.CTRL_MASK));
+        menuAviones.add(itemConsultasAviones);
+        itemConsultasAviones.addActionListener(this);
 
         menuBar.add(menuAviones);
 
 
-
-
 //----------------------- MENU EMPLEADOS ----------------------------
-            menuEmpleados = new JMenu("Empleados");
+        menuEmpleados = new JMenu("Empleados");
 
-                    menuTecnicos= new JMenu("Técnicos");
-                    menuTecnicos.setMnemonic(KeyEvent.VK_Q);
+        menuTecnicos = new JMenu("Técnicos");
+        menuTecnicos.setMnemonic(KeyEvent.VK_Q);
 
-                        itemAltasTecnicos = new JMenuItem("Agregar");
-                        menuTecnicos.add(itemAltasTecnicos);
-                        itemAltasTecnicos.addActionListener(this);
+        itemAltasTecnicos = new JMenuItem("Agregar");
+        menuTecnicos.add(itemAltasTecnicos);
+        itemAltasTecnicos.addActionListener(this);
 
-                        itemBajasTecnicos = new JMenuItem("Eliminar");
-                        menuTecnicos.add(itemBajasTecnicos);
-                        itemBajasTecnicos.addActionListener(this);
+        itemBajasTecnicos = new JMenuItem("Eliminar");
+        menuTecnicos.add(itemBajasTecnicos);
+        itemBajasTecnicos.addActionListener(this);
 
-                        itemModificacionesTecnicos = new JMenuItem("Modificar");
-                        menuTecnicos.add(itemModificacionesTecnicos);
-                        itemModificacionesTecnicos.addActionListener(this);
+        itemModificacionesTecnicos = new JMenuItem("Modificar");
+        menuTecnicos.add(itemModificacionesTecnicos);
+        itemModificacionesTecnicos.addActionListener(this);
 
-                        itemConsultasTecnicos = new JMenuItem("Buscar");
-                        menuTecnicos.add(itemConsultasTecnicos);
-                        itemConsultasTecnicos.addActionListener(this);
+        itemConsultasTecnicos = new JMenuItem("Buscar");
+        menuTecnicos.add(itemConsultasTecnicos);
+        itemConsultasTecnicos.addActionListener(this);
 
-                menuEmpleados.add(menuTecnicos);
+        menuEmpleados.add(menuTecnicos);
 
-                        menuTraficadores= new JMenu("Controladores Aéreos");
+        menuTraficadores = new JMenu("Controladores Aéreos");
 
-                        itemAltasTraficadores = new JMenuItem("Agregar");
-                        menuTraficadores.add(itemAltasTraficadores);
-                        itemAltasTraficadores.addActionListener(this);
+        itemAltasTraficadores = new JMenuItem("Agregar");
+        menuTraficadores.add(itemAltasTraficadores);
+        itemAltasTraficadores.addActionListener(this);
 
-                        itemBajasTraficadores = new JMenuItem("Eliminar");
-                        menuTraficadores.add(itemBajasTraficadores);
-                        itemBajasTraficadores.addActionListener(this);
+        itemBajasTraficadores = new JMenuItem("Eliminar");
+        menuTraficadores.add(itemBajasTraficadores);
+        itemBajasTraficadores.addActionListener(this);
 
-                        itemModificacionesTraficadores = new JMenuItem("Modificar");
-                        menuTraficadores.add(itemModificacionesTraficadores);
-                        itemModificacionesTraficadores.addActionListener(this);
+        itemModificacionesTraficadores = new JMenuItem("Modificar");
+        menuTraficadores.add(itemModificacionesTraficadores);
+        itemModificacionesTraficadores.addActionListener(this);
 
-                        itemConsultasTraficadores = new JMenuItem("Buscar");
-                        menuTraficadores.add(itemConsultasTraficadores);
-                        itemConsultasTraficadores.addActionListener(this);
+        itemConsultasTraficadores = new JMenuItem("Buscar");
+        menuTraficadores.add(itemConsultasTraficadores);
+        itemConsultasTraficadores.addActionListener(this);
 
-                        menuEmpleados.add(menuTraficadores);
+        menuEmpleados.add(menuTraficadores);
 
-            menuBar.add(menuEmpleados);
+        menuBar.add(menuEmpleados);
 
 
         setJMenuBar(menuBar);
@@ -120,31 +122,31 @@ public class VentanaInicio extends JFrame implements ActionListener {
 
         toolbar = new JToolBar(JToolBar.VERTICAL);
 
-            JLabel logo= new JLabel();
-            logo.setHorizontalAlignment(SwingConstants.CENTER);
-            logo.setIcon(new ImageIcon("./imagenes/logo.png"));
-            logo.setPreferredSize(new Dimension(150, 150));
-            logo.setAlignmentX(Component.CENTER_ALIGNMENT);
-            toolbar.setBackground(new Color(7, 41, 50));
-            toolbar.add(logo);
+        JLabel logo = new JLabel();
+        logo.setHorizontalAlignment(SwingConstants.CENTER);
+        logo.setIcon(new ImageIcon("./imagenes/logo.png"));
+        logo.setPreferredSize(new Dimension(150, 150));
+        logo.setAlignmentX(Component.CENTER_ALIGNMENT);
+        toolbar.setBackground(new Color(7, 41, 50));
+        toolbar.add(logo);
 
-            //btn consulta (jframe predeterminado) -------------
-            ImageIcon iconoConsulta = new ImageIcon("./imagenes/consultas.png");
-            Image imagenConsulta = iconoConsulta.getImage().getScaledInstance(64, 64, Image.SCALE_SMOOTH);
-            ImageIcon iconoConsultaEscalado = new ImageIcon(imagenConsulta);
+        //btn consulta (jframe predeterminado) -------------
+        ImageIcon iconoConsulta = new ImageIcon("./imagenes/consultas.png");
+        Image imagenConsulta = iconoConsulta.getImage().getScaledInstance(64, 64, Image.SCALE_SMOOTH);
+        ImageIcon iconoConsultaEscalado = new ImageIcon(imagenConsulta);
 
-            btnConsulta = new JButton(iconoConsultaEscalado);
-            btnConsulta.setBackground(new Color(7, 41, 50));
-            btnConsulta.setToolTipText("Consultar");
-            btnConsulta.setBorderPainted(false);
-            btnConsulta.setFocusPainted(false);
-            btnConsulta.setContentAreaFilled(true);
-            btnConsulta.setPreferredSize(new Dimension(64, 64));
-            btnConsulta.setAlignmentX(Component.CENTER_ALIGNMENT);
-            btnConsulta.addActionListener(this);
+        btnConsulta = new JButton(iconoConsultaEscalado);
+        btnConsulta.setBackground(new Color(7, 41, 50));
+        btnConsulta.setToolTipText("Consultar");
+        btnConsulta.setBorderPainted(false);
+        btnConsulta.setFocusPainted(false);
+        btnConsulta.setContentAreaFilled(true);
+        btnConsulta.setPreferredSize(new Dimension(64, 64));
+        btnConsulta.setAlignmentX(Component.CENTER_ALIGNMENT);
+        btnConsulta.addActionListener(this);
 
 
-            //btn agregar-----------
+        //btn agregar-----------
         ImageIcon iconoAgregar = new ImageIcon("./imagenes/agregar.png");
         Image imagenAgregar = iconoAgregar.getImage().getScaledInstance(64, 64, Image.SCALE_SMOOTH);
         ImageIcon iconoAgregarEscalado = new ImageIcon(imagenAgregar);
@@ -192,41 +194,106 @@ public class VentanaInicio extends JFrame implements ActionListener {
         btnModificar.addActionListener(this);
 
 
-            //btn para salir --------------
-            ImageIcon iconoSalir = new ImageIcon("./imagenes/salir.png");
-            Image imagenSalir = iconoSalir.getImage().getScaledInstance(64, 64, Image.SCALE_SMOOTH);
-            ImageIcon iconoEscalado = new ImageIcon(imagenSalir);
+        //btn para salir --------------
+        ImageIcon iconoSalir = new ImageIcon("./imagenes/salir.png");
+        Image imagenSalir = iconoSalir.getImage().getScaledInstance(64, 64, Image.SCALE_SMOOTH);
+        ImageIcon iconoEscalado = new ImageIcon(imagenSalir);
 
-            btnToolbarSalir = new JButton(iconoEscalado);
-            btnToolbarSalir.setBackground(new Color(7, 41, 50));
-            btnToolbarSalir.setToolTipText("Salir del programa.");
-            btnToolbarSalir.setBorderPainted(false);
-            btnToolbarSalir.setFocusPainted(false);
-            btnToolbarSalir.setContentAreaFilled(true);
-            btnToolbarSalir.setPreferredSize(new Dimension(64, 64));
-            btnToolbarSalir.setAlignmentX(Component.CENTER_ALIGNMENT);
-            btnToolbarSalir.addActionListener(this);
+        btnToolbarSalir = new JButton(iconoEscalado);
+        btnToolbarSalir.setBackground(new Color(7, 41, 50));
+        btnToolbarSalir.setToolTipText("Salir del programa.");
+        btnToolbarSalir.setBorderPainted(false);
+        btnToolbarSalir.setFocusPainted(false);
+        btnToolbarSalir.setContentAreaFilled(true);
+        btnToolbarSalir.setPreferredSize(new Dimension(64, 64));
+        btnToolbarSalir.setAlignmentX(Component.CENTER_ALIGNMENT);
+        btnToolbarSalir.addActionListener(this);
 
-            toolbar.add(btnConsulta);
-            toolbar.add(Box.createVerticalStrut(30));
-            toolbar.add(btnAgregar);
-            toolbar.add(Box.createVerticalStrut(30));
-            toolbar.add(btnBorrar);
-            toolbar.add(Box.createVerticalStrut(30));
-            toolbar.add(btnModificar);
-            toolbar.add(Box.createVerticalStrut(30));
-            toolbar.add(btnToolbarSalir);
+        toolbar.add(btnConsulta);
+        toolbar.add(Box.createVerticalStrut(30));
+        toolbar.add(btnAgregar);
+        toolbar.add(Box.createVerticalStrut(30));
+        toolbar.add(btnBorrar);
+        toolbar.add(Box.createVerticalStrut(30));
+        toolbar.add(btnModificar);
+        toolbar.add(Box.createVerticalStrut(30));
+        toolbar.add(btnToolbarSalir);
 
 
-            toolbar.setFloatable(false);
-            toolbar.setLayout(new BoxLayout(toolbar, BoxLayout.Y_AXIS));
-            toolbar.add(Box.createVerticalGlue());
-            add(toolbar, BorderLayout.WEST);
-            toolbar.setVisible(true);
+        toolbar.setFloatable(false);
+        toolbar.setLayout(new BoxLayout(toolbar, BoxLayout.Y_AXIS));
+        toolbar.add(Box.createVerticalGlue());
+        add(toolbar, BorderLayout.WEST);
+        toolbar.setVisible(true);
+
+
+        //agregado del desktop pane dentro de la ventana ------------
+        ventanaDesktopPane = new JDesktopPane();
+        add(ventanaDesktopPane, BorderLayout.CENTER);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        Object componente = e.getSource();
+
+
+        if (componente == itemAltasAviones) {
+                    JInternalFrame ventana = AltasAviones.getInstancia().getVentana();
+                    agregarVentanaSingelton(ventana);
+
+        }
+
+        if (componente == btnToolbarSalir) {
+            System.exit(0);
+        }
+/*
+        if (componente == itemBajas) {
+            SwingUtilities.invokeLater(new Runnable() {
+                @Override
+                public void run() {
+                    new VentanaBajas();
+                }
+            });
+        }
+        if (componente == itemModificaciones) {
+            SwingUtilities.invokeLater(new Runnable() {
+                @Override
+                public void run() {
+                    new VentanaModificaciones();
+                }
+            });
+        }
+        if (componente == itemConsultas) {
+            SwingUtilities.invokeLater(new Runnable() {
+                @Override
+                public void run() {
+                    new VentanaConsultas();
+                }
+            });
+        }
+
+ */
 
     }
+
+    private void agregarVentanaSingelton(JInternalFrame ventana) {
+        if (ventana == null) {
+            JOptionPane.showMessageDialog(this, "La ventana no está inicializada.");
+            return;
+        }
+
+        if (!ventana.isVisible()) {
+            ventanaDesktopPane.add(ventana);
+            ventana.setVisible(true);
+            ventana.toFront();
+        } else {
+            try {
+                ventana.setSelected(true);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
+
+
--- src/edu/tecjerez/topicos/vista/aviones/AltasAviones.java
+++ src/edu/tecjerez/topicos/vista/aviones/AltasAviones.java
@@ -1,5 +1,157 @@
 package edu.tecjerez.topicos.vista.aviones;
 
-public class AltasAviones {
+import javax.swing.*;
+import java.awt.*;
 
+public class AltasAviones {
+    public static AltasAviones instancia;
+    JInternalFrame ventanaAltas;
+    JDesktopPane desktopPaneAltas;
+
+
+    public AltasAviones(){
+        this.desktopPaneAltas = new JDesktopPane();
+        ventanaAltas = new JInternalFrame();
+        ventanaAltas.setSize(500, 700);
+        ventanaAltas.setClosable(true);
+        ventanaAltas.setIconifiable(true);
+        ventanaAltas.setResizable(true);
+        ventanaAltas.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        ventanaAltas.getContentPane().setLayout(new GridLayout());
+
+        JPanel panelCampos = new JPanel(new GridLayout(15, 2, 5, 5));
+        panelCampos.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        panelCampos.setBackground(new Color(181, 213, 183));
+
+        // 1. Número de registro
+        panelCampos.add(new JLabel("Número de registro:"));
+        JTextField campoRegistro = new JTextField();
+        panelCampos.add(campoRegistro);
+
+        // 2. Modelo de avión
+        panelCampos.add(new JLabel("Modelo de avión:"));
+        JTextField campoModelo = new JTextField();
+        panelCampos.add(campoModelo);
+
+        // 3. Capacidad de peso
+        panelCampos.add(new JLabel("Capacidad de peso en kilogramos:"));
+        JTextField campoPeso = new JTextField();
+        panelCampos.add(campoPeso);
+
+        // 4. Examen 1 - Velocidad
+        JCheckBox checkExamen1 = new JCheckBox("Examen 1: Velocidad (especificar calificación en el campo)");
+        panelCampos.add(checkExamen1);
+
+        // 5. Calificación 1
+        SpinnerNumberModel modeloCal1 = new SpinnerNumberModel(0, 0, 100, 1);
+        JSpinner spinnerCal1 = new JSpinner(modeloCal1);
+        spinnerCal1.setEnabled(false);
+        panelCampos.add(spinnerCal1);
+
+        // 6. Examen 2 - Altitud Máxima
+        JCheckBox checkExamen2 = new JCheckBox("Examen 2: Altitud Máxima (especificar calificación en el campo");
+        panelCampos.add(checkExamen2);
+
+        // 7. Calificación 2
+        SpinnerNumberModel modeloCal2 = new SpinnerNumberModel(0, 0, 100, 1);
+        JSpinner spinnerCal2 = new JSpinner(modeloCal2);
+        spinnerCal2.setEnabled(false);
+        panelCampos.add(spinnerCal2);
+
+        // 8. SSN técnico Examen 1
+        panelCampos.add(new JLabel("NSS técnico Examen 1:"));
+        JTextField campoSSN1 = new JTextField();
+        campoSSN1.setEnabled(false);
+        panelCampos.add(campoSSN1);
+
+        // 9. Nombre técnico Examen 1
+        panelCampos.add(new JLabel("Nombre técnico Examen 1:"));
+        JTextField campoNombreTec1 = new JTextField();
+        campoNombreTec1.setEnabled(false);
+        panelCampos.add(campoNombreTec1);
+
+        // 10. SSN técnico Examen 2
+        panelCampos.add(new JLabel("NSS técnico Examen 2:"));
+        JTextField campoSSN2 = new JTextField();
+        campoSSN2.setEnabled(false);
+        panelCampos.add(campoSSN2);
+
+        // 11. Nombre técnico Examen 2
+        panelCampos.add(new JLabel("Nombre técnico Examen 2:"));
+        JTextField campoNombreTec2 = new JTextField();
+        campoNombreTec2.setEnabled(false);
+        panelCampos.add(campoNombreTec2);
+
+        // 12. Fecha examen 1
+        panelCampos.add(new JLabel("Fecha Examen 1:"));
+        JPanel panelFecha1 = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        JSpinner dia1 = new JSpinner(new SpinnerNumberModel(1, 1, 31, 1));
+        JSpinner mes1 = new JSpinner(new SpinnerNumberModel(1, 1, 12, 1));
+        JSpinner año1 = new JSpinner(new SpinnerNumberModel(2024, 1900, 2100, 1));
+        dia1.setEnabled(false); mes1.setEnabled(false); año1.setEnabled(false);
+        panelFecha1.add(dia1); panelFecha1.add(mes1); panelFecha1.add(año1);
+        panelCampos.add(panelFecha1);
+
+        // 13. Fecha examen 2
+        panelCampos.add(new JLabel("Fecha Examen 2:"));
+        JPanel panelFecha2 = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        JSpinner dia2 = new JSpinner(new SpinnerNumberModel(1, 1, 31, 1));
+        JSpinner mes2 = new JSpinner(new SpinnerNumberModel(1, 1, 12, 1));
+        JSpinner año2 = new JSpinner(new SpinnerNumberModel(2024, 1900, 2100, 1));
+        dia2.setEnabled(false); mes2.setEnabled(false); año2.setEnabled(false);
+        panelFecha2.add(dia2); panelFecha2.add(mes2); panelFecha2.add(año2);
+        panelCampos.add(panelFecha2);
+
+        // 14. Tiempo en aire examen 1
+        panelCampos.add(new JLabel("Tiempo en aire examen 1 (min):"));
+        JSpinner tiempo1 = new JSpinner(new SpinnerNumberModel(0, 0, 999, 1));
+        tiempo1.setEnabled(false);
+        panelCampos.add(tiempo1);
+
+        // 15. Tiempo en aire examen 2
+        panelCampos.add(new JLabel("Tiempo en aire examen 2 (min):"));
+        JSpinner tiempo2 = new JSpinner(new SpinnerNumberModel(0, 0, 999, 1));
+        tiempo2.setEnabled(false);
+        panelCampos.add(tiempo2);
+
+        // Agregar lógica para habilitar campos cuando se seleccionen los checkboxes
+        checkExamen1.addActionListener(e -> {
+            boolean activo = checkExamen1.isSelected();
+            spinnerCal1.setEnabled(activo);
+            campoSSN1.setEnabled(activo);
+            campoNombreTec1.setEnabled(activo);
+            dia1.setEnabled(activo);
+            mes1.setEnabled(activo);
+            año1.setEnabled(activo);
+            tiempo1.setEnabled(activo);
+        });
+
+        checkExamen2.addActionListener(e -> {
+            boolean activo = checkExamen2.isSelected();
+            spinnerCal2.setEnabled(activo);
+            campoSSN2.setEnabled(activo);
+            campoNombreTec2.setEnabled(activo);
+            dia2.setEnabled(activo);
+            mes2.setEnabled(activo);
+            año2.setEnabled(activo);
+            tiempo2.setEnabled(activo);
+        });
+
+        ventanaAltas.add(panelCampos, BorderLayout.CENTER);
+        ventanaAltas.pack();
+    }
+
+    //altas aviones singleton verificacion ----------
+
+    public static AltasAviones getInstancia() {
+        if (instancia == null) {
+            instancia = new AltasAviones();
+        }
+        return instancia;
+    }
+
+    public JInternalFrame getVentana() {
+        return ventanaAltas;
+    }
 }
+
